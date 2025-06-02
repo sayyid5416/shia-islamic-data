3192b1bb1db5b99a04675f9190101a0dfa5d380f
sayyid5416 <hussainabbas5416@outlook.com>
2025-06-02 17:03:21 +0530

    Refactor file handling in convert.py and add .gitignore for temporary files

--- convert.py
+++ convert.py
@@ -1,22 +1,40 @@
 """Reads lines from raw.txt and writes them with total_lines to result.json."""
-import json
+import json, os
 
 
 def printStart(msg: str):
     print(f"> {msg}")
     
 def printDone(msg: str):
-    print(f"⩥ {msg}\n")
+    print(f"    ⩥ {msg}")
     
 def printError(msg: str):
-    print(f"[x] {msg}\n")
+    print(f"    [x] {msg}")
 
 
+def prepare_file(filePath: str):
+    # Directory
+    dirPath = os.path.dirname(filePath)
+    if dirPath and not os.path.exists(dirPath):
+        printStart(f"Directory '{dirPath}' not found - creating it.")
+        os.makedirs(dirPath, exist_ok=True)
+        printDone(f"Created directory: {dirPath}")
 
-def convert_text_to_json(input_file: str = "raw.txt", output_file: str = "result.json") -> None:
+    # File
+    if not os.path.exists(filePath):
+        printStart(f"File '{filePath}' not found - creating it.")
+        with open(filePath, "w", encoding="utf-8") as f:
+            pass
+        printDone(f"Created empty file: {filePath}")
+    
+    
+def extract_text(inputFile: str, outputFile: str):
     try:
-        printStart(f"Reading from {input_file}...")
-        with open(input_file, "r", encoding="utf-8") as f:
+        prepare_file(inputFile)
+        prepare_file(outputFile)
+        
+        printStart(f"Reading from {inputFile}...")
+        with open(inputFile, "r", encoding="utf-8") as f:
             lines = [line.strip() for line in f if line.strip()]
         printDone(f"Total non-empty lines read: {len(lines)}")
 
@@ -25,13 +43,13 @@ def convert_text_to_json(input_file: str = "raw.txt", output_file: str = "result
             "text": lines
         }
 
-        printStart(f"Writing result to {output_file}...")
-        with open(output_file, "w", encoding="utf-8") as f:
+        printStart(f"Writing result to {outputFile}...")
+        with open(outputFile, "w") as f:
             json.dump(result, f, ensure_ascii=False, indent=4)
         printDone("Done.")
         
     except FileNotFoundError:
-        printError(f"Error: File '{input_file}' not found.")
+        printError(f"Error: File '{inputFile}' not found.")
         
     except IOError as e:
         printError(f"I/O error({e.errno}): {e.strerror}")
@@ -41,4 +59,7 @@ def convert_text_to_json(input_file: str = "raw.txt", output_file: str = "result
 
 
 if __name__ == "__main__":
-    convert_text_to_json()
+    extract_text(
+        inputFile = ".temp/raw.txt",
+        outputFile = ".temp/results.json"
+    )
