84ead49131c3db4846faab9d0c7e5687b8be4835
sayyid5416 <hussainabbas5416@outlook.com>
2025-06-12 21:40:25 +0530

    added dua kumail

--- helper_ziyarah.py
+++ helper_ziyarah.py
@@ -4,8 +4,17 @@ import json, os
 
 
 # ITEM
-ZIYARAH_NAME = "Tasbih - Syeda Fatima az-Zahra (sa)"
+ZIYARAH_NAME = "Dua Kumayl"
 DESCRIPTION = """
+Kumayl Ibn Ziyad Nakha'i was a confidant amongst the companions of Imam Ali Ibn Abi Talib (as) and this sublime Du'a was first heard from the beautiful, though anguished, voice of Imam Ali.
+
+According to Allama Majlisi, Kumayl had attended an assembly in the Mosque at Basra which was addressed by Imam Ali in the course of which the night of the 15th of Shaban was mentioned. 
+
+Imam Ali said - "Whosoever keeps awake in devoutness on this night and recites the Du'a of Prophet Khizr, undoubtedly that person's supplication will be responded to and granted."
+
+When the assembly at the Mosque had dispersed, Kumayl called at the house where Imam Ali was staying, and requested him to acquaint him with Prophet Khizr's "Du'a". Imam Ali asked Kumayl to sit down, record and memorise the "Du'a" which Imam Ali dictated to Kumayl.
+
+Imam Ali then advised Kumayl to recite this "Du'a" on the eve of (i.e. evening preceding) every Friday, or once a month or at least once in every year so that, added Imam Ali, "Allah may protect thee from the evils of the enemies and the plots contrived by impostors. O' Kumayl! in consideration of thy companionship and understanding, I grant thee this honour of entrusting this "Du'a" to thee."
 """
 LANGUAGES = ["ar", "transliteration", "en"]
 
@@ -15,12 +24,12 @@ LANGUAGES = ["ar", "transliteration", "en"]
 # TEXT_DIR = "ziyarah/text"
 
 # PATH - Dhikr
-INDEX_JSON_PATH = "dhikr/index.json"
-TEXT_DIR = "dhikr/text"
+# INDEX_JSON_PATH = "dhikr/index.json"
+# TEXT_DIR = "dhikr/text"
 
-# PATH - Sermon
-# INDEX_JSON_PATH = "sermon/index.json"
-# TEXT_DIR = "sermon/text"
+# PATH - Duas
+INDEX_JSON_PATH = "duas/index.json"
+TEXT_DIR = "duas/text"
 
 
 
