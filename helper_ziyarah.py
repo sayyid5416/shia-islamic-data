59da8ba41e6bd0ccf5dcff6972807681945e0ba5
sayyid5416 <hussainabbas5416@outlook.com>
2025-06-24 18:06:55 +0530

    interlinks

--- helper_ziyarah.py
+++ helper_ziyarah.py
@@ -4,11 +4,11 @@ import json, os
 
 
 # ITEM
-ZIYARAH_NAME = "Ashura Eve"
+ZIYARAH_NAME = "Eid al-Ghadeer"
 DESCRIPTION = """
 """
 LANGUAGES = ["en"]
-LANGUAGES = ["ar", "transliteration", "en"]
+# LANGUAGES = ["ar", "transliteration", "en"]
 # FOLDER = "salah"
 # FOLDER = "duas"
 # FOLDER = "dhikr"
