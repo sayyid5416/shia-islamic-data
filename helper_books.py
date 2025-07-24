e95ea42bb8fee6f2f1c3bbf84ba0440cd7475b4c
sayyid5416 <hussainabbas5416@outlook.com>
2025-07-24 18:13:30 +0530

    Sahih Al-Bukhari - Volume 7

--- helper_books.py
+++ helper_books.py
@@ -6,7 +6,7 @@ import os
 
 
 # Information
-NAME = "Sahih Al-Bukhari - Volume 6"
+NAME = "Sahih Al-Bukhari - Volume 7"
 AUTHOR = "Abu Abdullah Muhammad ibn Ismail al-Bukhari"
 TRANSLATOR = ["Dr. Muhammad Muhsin Khan"]
 PUBLISHER = "Darussalam Publishers and Distributors, Riyadh, Saudi Arabia"
